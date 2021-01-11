2436d5e0d4ec3146a33fc3b9398717431e4c2cc0
anproagile <anproagile@gmail.com>
2021-01-11 16:06:09 -0600

    Remove unused variables

--- app/src/components/Header.js
+++ app/src/components/Header.js
@@ -2,10 +2,9 @@ import React from 'react';
 
 import cs from 'clsx';
 import s from './Header.module.css';
-import awsmllogo from '../img/logo_awsml_01.svg';
 
 import { STAGE_HOME, STAGE_TRANSCRIBING, STAGE_TRANSCRIBED, STAGE_SUMMARIZE, STAGE_EXPORT, STAGE_SEARCH_EXPORT, STAGE_SEARCH } from '../consts';
-import { Link, useHistory } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import { Auth } from "aws-amplify";
 
 export default function Header({
@@ -38,7 +37,7 @@ export default function Header({
             <button onClick={onHideExport}><span />Back</button>
           : null}
 
-          <a href="https://aws.amazon.com/machine-learning/ "><img className={s.logo} src={awsmllogo} /></a>
+          <a href="https://aws.amazon.com/machine-learning/ "><img className={s.logo} src={require('../img/logo_awsml_01.svg')} /></a>
         </div>
         <div className={s.headings}>
         <h1>Medical Transcription Analysis</h1> 
--- app/src/temp.js
+++ app/src/temp.js
@@ -278,7 +278,7 @@ export default function Home() {
   const handleSave = (e) => {
     e.preventDefault()
     setShowCreateSessionForm(true)
-    toggleShowForm()
+    toogleShowForm()
   }
 
   const handleSessionSubmit = (e) => {
